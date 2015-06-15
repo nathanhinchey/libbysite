6e6991977f06c2f1b8d17740b111c37f39a39eb6
nathanhinchey <devnph@gmail.com>
2015-06-15 14:11:16 -0400

    Working on debugging.

--- assets/lib/scripts/libbysite.js
+++ assets/lib/scripts/libbysite.js
@@ -8,7 +8,7 @@
     initialize: function(){
       new LibbySite.Views.Home({el: "#main-section"});
 
-      new LibbySite.Routers.Router({$el: $("#main-section")});
+      var LibbySite.router = new LibbySite.Routers.Router({$el: $("#main-section")});
       Backbone.history.start();
     }
   };
